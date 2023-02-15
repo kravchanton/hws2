2f7a22d15141b0ba1db5680fc391c5db06f5da70
Kravchuk Anton <Kravch-anton@mail.ru>
2023-02-15 22:16:29 +0300

    small fix

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -18,19 +18,17 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                <Route path="/" element={<PreJunior/>}>
+                    <Route path="/" element={<PreJunior/>}/>
                     <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                     <Route path={PATH.JUNIOR} element={<Junior/>}/>
                     <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
-                </Route>
 
-
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                <Route path='*' element={<Error404/>}/>
+                    {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
+                    <Route path='*' element={<Error404/>}/>
             </Routes>
         </div>
-    )
+)
 }
 
 export default Pages
