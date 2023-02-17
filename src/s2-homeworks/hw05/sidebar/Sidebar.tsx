504a2f5566094066efb139adf76c639803b4adb9
Kravchuk Anton <Kravch-anton@mail.ru>
2023-02-17 23:00:59 +0300

    fix active style

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -31,8 +31,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={({ isActive }) =>
-                            isActive ? s.active : undefined}
+                        className={({ isActive }) => (isActive ? s.active : '')}
                     >
                         Pre-junior
                     </NavLink>
@@ -40,8 +39,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        className={({ isActive }) =>
-                            isActive ? s.active : undefined}
+                        className={({ isActive }) => (isActive ? s.active : '')}
                     >
                         Junior
                     </NavLink>
@@ -49,8 +47,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        className={({ isActive }) =>
-                            isActive ? s.active : undefined}
+                        className={({ isActive }) => (isActive ? s.active : '')}
                     >
                         Junior Plus
                     </NavLink>
